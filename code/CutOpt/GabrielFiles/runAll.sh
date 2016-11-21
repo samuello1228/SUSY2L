2c1fcaa0e60705cf5dc9c88abc5bd676d67b3cd2
Gabriel Gallardo <gabriel.gallardo@cern.ch>
2016-11-21 16:06:03 +0800

    Fix bugs in runAll.sh and optWithTMVA.py

--- code/CutOpt/GabrielFiles/runAll.sh
+++ code/CutOpt/GabrielFiles/runAll.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-channels="0 1 2 10 11 12"
-masses="20 50 100 +"
+channels="0" #1 2 10 11 12"
+masses="20" # 50 100 +"
 runJobs(){
 	for chan in $channels
 	do
 		for mass in $masses
 		do
-			python CutOpt/optWithTMVA.py -b GabrielFiles/bkgFiles.txt -s GabrielFiles/sigFiles_$mass.txt -d GabrielFiles/dataFiles.txt -c $channel -o Channel_$chan_dm$mass.root;
+			python CutOpt/optWithTMVA.py -b CutOpt/GabrielFiles/bkgFiles.txt -s CutOpt/GabrielFiles/sigFiles_$mass.txt -d CutOpt/GabrielFiles/dataFiles.txt -c $chan -o Channel_$chan_dm$mass.root;
 		done
 	done
 }
