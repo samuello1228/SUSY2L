9da7d9f86cc5c141266c97edfb052b0d65ec6b87
Gabriel Gallardo <gabriel.gallardo@cern.ch>
2016-11-22 08:39:35 +0800

    Update runAll.sh to take advantage of new options in optWithTMVA.py

--- code/CutOpt/GabrielFiles/runAll.sh
+++ code/CutOpt/GabrielFiles/runAll.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 
-channels="2 10 11 12"
+channels="1 2 10 11 12"
 masses="20 50 100 +"
+nTreesList="600 1000" # 400"
+nodeSizeList"5 7 10"
 
 bkgFiles="CutOpt/GabrielFiles/bkgFiles.txt"
 dataFiles="CutOpt/GabrielFiles/dataFiles.txt"
 
-for chan in $channels
+for nTrees in $nTreesList; do; for nodeSize in $nodeSizeList
 do
+	for chan in $channels
+	do
+		for mass in $masses
+		do
+		    outFile="Channel"$chan"_dm"$mass".root"
+		    sigFiles="CutOpt/GabrielFiles/sigFiles_"$mass".txt"
+		    python CutOpt/optWithTMVA.py -b $bkgFiles -s $sigFiles -d $dataFiles -c $chan -o $outFile&
+		    echo "Submitted jobs for channel "$chan" dm="$mass
+		done
+		wait
+	done
+
+	# Channel 0 
 	for mass in $masses
 	do
-	    outFile="Channel"$chan"_dm"$mass".root"
-	    sigFiles="CutOpt/GabrielFiles/sigFiles_"$mass".txt"
-	    python CutOpt/optWithTMVA.py -b $bkgFiles -s $sigFiles -d $dataFiles -c $chan -o $outFile&
-	    echo "Submitted jobs for channel "$chan" dm="$mass
+		outFile="Channel"0"_dm"$mass".root"
+		sigFiles="CutOpt/GabrielFiles/sigFiles_"$mass".txt"
+		python CutOpt/optWithTMVA.py -b $bkgFiles -s $sigFiles -d $dataFiles -c 0 -o $outFile&
 	done
 	wait
-done
-
-for mass in $masses
-do
-	outFile="Channel"0"_dm"$mass".root"
-	sigFiles="CutOpt/GabrielFiles/sigFiles_"$mass".txt"
-	python CutOpt/optWithTMVA.py -b $bkgFiles -s $sigFiles -d $dataFiles -c 0 -o $outFile&
-done
-
-wait
+	dirName="Output_"$nTrees"_NodeSize"$nodeSize
+	mkdir $dirName
+	mv Channel*.root $dirName
+done; done
 
 echo "All done!"
