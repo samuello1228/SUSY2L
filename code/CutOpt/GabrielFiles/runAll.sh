6d4071fa79de7c84b855e3e6048da539e805d767
Gabriel Gallardo <gabriel.gallardo@cern.ch>
2016-11-22 09:28:14 +0800

    Fix bugs in runAll.sh

--- code/CutOpt/GabrielFiles/runAll.sh
+++ code/CutOpt/GabrielFiles/runAll.sh
@@ -3,12 +3,12 @@
 channels="1 2 10 11 12"
 masses="20 50 100 +"
 nTreesList="400 600 1000"
-nodeSizeList"5 7 10"
+nodeSizeList="5 7 10"
 
 bkgFiles="CutOpt/GabrielFiles/bkgFiles.txt"
 dataFiles="CutOpt/GabrielFiles/dataFiles.txt"
 
-for nTrees in $nTreesList; do; for nodeSize in $nodeSizeList
+for nTrees in $nTreesList; do for nodeSize in $nodeSizeList
 do
 	for chan in $channels
 	do
@@ -17,7 +17,7 @@ do
 		    outFile="Channel"$chan"_dm"$mass".root"
 		    sigFiles="CutOpt/GabrielFiles/sigFiles_"$mass".txt"
 		    python CutOpt/optWithTMVA.py -b $bkgFiles -s $sigFiles -d $dataFiles -c $chan -o $outFile --NTrees $nTrees --NodeSize $nodeSize&
-		    echo "Submitted jobs for channel "$chan" dm="$mass
+		    echo "Submitted jobs for channel "$chan" dm="$mass" nTrees="$nTrees" nodeSize="$nodeSize
 		done
 		wait
 	done
@@ -28,6 +28,7 @@ do
 		outFile="Channel"0"_dm"$mass".root"
 		sigFiles="CutOpt/GabrielFiles/sigFiles_"$mass".txt"
 		python CutOpt/optWithTMVA.py -b $bkgFiles -s $sigFiles -d $dataFiles -c 0 -o $outFile --NTrees $nTrees --NodeSize $nodeSize&
+		echo "Submitted jobs for channel 0 dm="$mass" nTrees="$nTrees" nodeSize="$nodeSize
 	done
 	wait
 	dirName="Output_"$nTrees"_NodeSize"$nodeSize
