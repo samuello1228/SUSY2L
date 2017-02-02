a73a6edab50e04014dfcb6c3b8f95719283f0331
Samuel <locheukyee2001@gmail.com>
2017-02-02 23:18:49 +0100

    prepare to submit jobs

--- code/multiLepSearch/script/Data_submit.sh
+++ code/multiLepSearch/script/Data_submit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-tag=v8.8
+tag=v8.10
 dataPRW=multiLepSearch/prw_Data/ilumicalc_histograms_None_297730-311481_OflLumi-13TeV-005.root
 grl=multiLepSearch/GRL/physics_25ns_20.7.xml
-mcPRW=multiLepSearch/prw_MC/merged_prw_mc15c_July27_afterFix.root
+mcPRW=multiLepSearch/prw_MC/merged_prw_mc15c_Jan31.root
 file=../multiLepSearch/script/Data_sample_list.txt
 k=${tag}.data 
 ../multiLepSearch/util/run_ss_selection.py --driver grid --inputList ${file} --dataPRW ${dataPRW} --mcPRW ${mcPRW} --grl ${grl} --outputTag ${tag} -o ${k} -w -a 0 --study ss --doSys 0
--- code/multiLepSearch/script/MC_submit.sh
+++ code/multiLepSearch/script/MC_submit.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-tag=v8.8
+tag=v8.10
 dataPRW=multiLepSearch/prw_Data/ilumicalc_histograms_None_297730-311481_OflLumi-13TeV-005.root
 
 #For background MC
-#mcPRW=multiLepSearch/prw_MC/merged_prw_mc15c.root
-mcPRW=multiLepSearch/prw_MC/merged_prw_mc15c_July27_afterFix.root
-# file=../multiLepSearch/script/MCBG_sample_list.txt
+#mcPRW=multiLepSearch/prw_MC/merged_prw_mc15c_July27_afterFix.root
+mcPRW=multiLepSearch/prw_MC/merged_prw_mc15c_Jan31.root
+#file=../multiLepSearch/script/MCBG_sample_list.txt
 #file=../multiLepSearch/script/MCBGZjetsSherpa_sample_list.txt
 #file=../multiLepSearch/script/MCBGVVSherpa_sample_list.txt
-# file=../multiLepSearch/script/MCBGVgammaSherpa_sample_list.txt
+#file=../multiLepSearch/script/MCBGVgammaSherpa_sample_list.txt
 #file=../multiLepSearch/script/MCBGZeeSherpaSelected.txt
-file=../multiLepSearch/script/MCBG_llll.txt
+#file=../multiLepSearch/script/MCBG_llll.txt
 
 k=${tag}.MCBG
 #k=${tag}.MCZee
@@ -20,4 +20,4 @@ k=${tag}.MCBG
 #file=../multiLepSearch/script/MCSig_sample_list.txt
 #k=${tag}.MCSig
 
-../multiLepSearch/util/run_ss_selection.py --driver direct --inputList ${file} --dataPRW ${dataPRW} --mcPRW ${mcPRW} --outputTag ${tag} -o ${k} -w -a 1 --study ss --doSys 0
+../multiLepSearch/util/run_ss_selection.py --driver grid --inputList ${file} --dataPRW ${dataPRW} --mcPRW ${mcPRW} --outputTag ${tag} -o ${k} -w -a 1 --study ss --mcMatch TruthLink --doSys 0
