ba3a9ff2f2a2b27a23525acf6383614444d09732
Gabriel Gallardo <gabriel.gallardo@cern.ch>
2017-02-24 14:28:49 +0100

    Rutime changes to getSSPrediction

--- code/multiLepSearch/ChargeFlipBkg/estimate/getSSPrediction.C
+++ code/multiLepSearch/ChargeFlipBkg/estimate/getSSPrediction.C
@@ -23,13 +23,13 @@
 // ========= CONFIGURATION =========== //
 TString defaultOut="outputDirYL-PtCorr";
 TString defaultNTupleList="../common/inFileList-DataYL.txt";
-//TString defaultMisIdfile="~/ChargeFlipBkg/chargeMisID/latest/80.000000_100.000000_20.000000_20.000000_DATA.root";
-TString defaultMisIdfile="../common/chargeMisID_Zee_data_signal_wSys.root";
+TString defaultMisIdfile="../QID-on/rates_wSys.root";
+// TString defaultMisIdfile="../common/chargeMisID_Zee_data_signal_wSys.root";
 //TString misIDhistname="80.0_100.0_20.0_20.0_DATA_misid";
-TString misIDhistname="hFlipProb_data";
+TString misIDhistname="hFlipProb_MCtruth";
 // TString misIDhistname="hFlipProb_MCLH";
 // TString misIDhistname="hFlipProb";
-TString defaultdPtfile="../common/dPT_signal.root";
+TString defaultdPtfile="../QID-on/ptcorr/dEhistos.root";
 bool onlySignal=true;
 bool applyPtCorrection=true;
 bool passQID=true;
