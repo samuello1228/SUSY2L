c6679976fc2a265f0cebed2abee89dd6a9d5bd07
Samuel <locheukyee2001@gmail.com>
2017-03-30 18:58:55 +0200

    use AnalysisBase 2.4.29 and new conf file

--- code/multiLepSearch/script/MC_submit.sh
+++ code/multiLepSearch/script/MC_submit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-tag=v8.13
+tag=v9.0
 dataPRW=GoodRunsLists/data16_13TeV/20161101/physics_25ns_20.7.lumicalc.OflLumi-13TeV-005.root,GoodRunsLists/data15_13TeV/20160720/physics_25ns_20.7.lumicalc.OflLumi-13TeV-005.root
+SUSYconf=multiLepSearch/sel_conf/SUSYTools_multilepAnaMoriond.conf
 
 #For background MC
 #mcPRW=dev/SUSYTools/merged_prw_mc15c_latest.root
@@ -16,12 +17,10 @@ file=../multiLepSearch/script/MCBG_sample_list.txt
 #file=../multiLepSearch/script/MCBG_llll.txt
 
 k=${tag}.MCBG
-#k=${tag}.MCZee
 
 #For signal MC
 #mcPRW=multiLepSearch/prw_MC/merged_prw_mc15c_Slep0d95.root,dev/PileupReweighting/mc15c_v2_defaults.NotRecommended.prw.root
 #file=../multiLepSearch/script/MCSig_sample_list.txt
 #k=${tag}.MCSig
 
-../multiLepSearch/util/run_ss_selection.py --driver grid --inputList ${file} --dataPRW ${dataPRW} --mcPRW ${mcPRW} --outputTag ${tag} -o ${k} -w -a 1 --study ss --mcMatch dR --doSys 0 --ChargeID 1
-
+../multiLepSearch/util/run_ss_selection.py --driver grid --inputList ${file} --dataPRW ${dataPRW} --mcPRW ${mcPRW} --outputTag ${tag} -o ${k} -w -a 1 --study ss --mcMatch dR --doSys 0 --ChargeID 1 --conf ${SUSYconf}
