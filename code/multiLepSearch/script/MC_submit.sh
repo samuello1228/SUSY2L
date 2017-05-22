71c02737064514c32f771e9500c8bae587e996c3
Samuel <locheukyee2001@gmail.com>
2017-05-23 00:59:12 +0200

    re-submit two VV samples

--- code/multiLepSearch/script/MC_submit.sh
+++ code/multiLepSearch/script/MC_submit.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-tag=v9.3
+tag=v9.3.1
 dataPRW=GoodRunsLists/data16_13TeV/20170215/physics_25ns_20.7.lumicalc.OflLumi-13TeV-008.root,GoodRunsLists/data15_13TeV/20160720/physics_25ns_20.7.lumicalc.OflLumi-13TeV-005.root
-mcPRW=dev/SUSYTools/merged_prw_mc15c_Jan31.root,multiLepSearch/prw_MC/merged_prw_mc15c_Slep0d95.root,dev/PileupReweighting/mc15c_v2_defaults.NotRecommended.prw.root
+#mcPRW=dev/SUSYTools/merged_prw_mc15c_Jan31.root,multiLepSearch/prw_MC/merged_prw_mc15c_Slep0d95.root,dev/PileupReweighting/mc15c_v2_defaults.NotRecommended.prw.root
+mcPRW=dev/PileupReweighting/mc15c_v2_defaults.NotRecommended.prw.root
 SUSYconf=multiLepSearch/sel_conf/SUSYTools_multilepAnaMoriond.conf
 
 #For background MC
 #mcPRW=dev/SUSYTools/merged_prw_mc15c_latest.root
 
-file=../multiLepSearch/script/MCBG_sample_list.txt,../multiLepSearch/script/MCBGZjetsSherpa_sample_list.txt,../multiLepSearch/script/MCBGVVSherpa_sample_list.txt,../multiLepSearch/script/MCBGVgammaSherpa_sample_list.txt,../multiLepSearch/script/MCBGDYSherpa_sample_list.txt
+#file=../multiLepSearch/script/MCBG_sample_list.txt,../multiLepSearch/script/MCBGZjetsSherpa_sample_list.txt,../multiLepSearch/script/MCBGVVSherpa_sample_list.txt,../multiLepSearch/script/MCBGVgammaSherpa_sample_list.txt,../multiLepSearch/script/MCBGDYSherpa_sample_list.txt
 #file=../multiLepSearch/script/MCBG_sample_list.txt
 #file=../multiLepSearch/script/MCBGZjetsSherpa_sample_list.txt
-#file=../multiLepSearch/script/MCBGVVSherpa_sample_list.txt
+file=../multiLepSearch/script/MCBGVVSherpa_sample_list.txt
 #file=../multiLepSearch/script/MCBGVgammaSherpa_sample_list.txt
 #file=../multiLepSearch/script/MCBGDYSherpa_sample_list.txt
 
