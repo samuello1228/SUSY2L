79dbacc35eee6bbccc092a2d9e9a180db0ab7f04
Gabriel Gallardo <gabriel.gallardo@cern.ch>
2017-05-23 06:14:25 +0200

    Simplify BDT runAll script
    Takes faar too long to exeucte all possible variations.
    Optimizing for ISR and nonISR channels only (3 and 13).
    Run for dm=20 and dm=50 only as default settings already work well with dm=100

--- code/CutOpt/GabrielFiles/runAll.sh
+++ code/CutOpt/GabrielFiles/runAll.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # channels="1 2 3 4 10 11 12 13 14 20 21 22 23 24"
-channels="1 2 3 4 10 11 12 13 14"
-masses="20 50 100"
+# channels="1 2 3 4 10 11 12 13 14"
+# masses="20 50 100"
 # masses1="all 20"
 # masses2="50 100 +"
 nTreesList="100 200 400 600"
 nodeSizeList="5 7 10"
 depthList="2 3 4"
 
-nTreesList="400"
-nodeSizeList="5"
-depthList="3"
+
+channels="3 13"
+masses="20 50"
 
 bkgFiles="CutOpt/GabrielFiles/MCbkgFiles.txt"
 dataFiles="CutOpt/GabrielFiles/data2016.txt"
@@ -33,19 +33,20 @@ do
 		    python CutOpt/optWithTMVA.py -b $bkgFiles -s $sigFiles -d $dataFiles -c $chan1 -o $outFile".root" --NTrees $nTrees --NodeSize $nodeSize --Depth $depth > $outFile".log"&
 		    echo "Submitted jobs for channel "$chan1" dm="$mass" nTrees="$nTrees" nodeSize="$nodeSize" depth="$depth
 		done
-		wait
+		# wait
 	done
+	wait
 
-	# Channel 0 
-    for mass in $masses
-    do 
-	    outFile="dm"$mass"_Channel0"
-		sigFiles="CutOpt/GabrielFiles/sigFiles_"$mass".txt"
-		python CutOpt/optWithTMVA.py -b $bkgFiles -s $sigFiles -d $dataFiles -c 0 -o $outFile".root" --NTrees $nTrees --NodeSize $nodeSize --Depth $depth > $outFile".log"&
-	    echo "Submitted jobs for channel 0 dm="$mass" nTrees="$nTrees" nodeSize="$nodeSize" depth="$depth
-    done 
+	# # Channel 0 
+	# for mass in $masses
+	# do 
+	# 	outFile="dm"$mass"_Channel0"
+	# 	sigFiles="CutOpt/GabrielFiles/sigFiles_"$mass".txt"
+	# 	python CutOpt/optWithTMVA.py -b $bkgFiles -s $sigFiles -d $dataFiles -c 0 -o $outFile".root" --NTrees $nTrees --NodeSize $nodeSize --Depth $depth > $outFile".log"&
+	# 	echo "Submitted jobs for channel 0 dm="$mass" nTrees="$nTrees" nodeSize="$nodeSize" depth="$depth
+	# done 
 
-    wait
+	# wait
 
 	dirName="Output_"$nTrees"_NodeSize"$nodeSize"_Depth"$depth
 	mkdir $dirName
