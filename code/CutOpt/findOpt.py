8aa5a855cdd2093f1a8072dc92c99e1265010c3d
Gabriel Gallardo <gabriel.gallardo@cern.ch>
2017-05-29 14:46:16 +0800

    Update findOpt script

--- code/CutOpt/findOpt.py
+++ code/CutOpt/findOpt.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 import os, sys, re, operator, overtraining, tqdm, subprocess, csv
-import ROOT
 from os import listdir, path, mkdir, rename
 from os.path import isdir, isfile
 from re import search
 from subprocess import call
 from tqdm import tqdm
+import ROOT
+from ROOT.Math import poisson_cdf as PP
 
 OTtolerance = 0.1
 
@@ -32,11 +33,13 @@ headersOT = [
 	]
 
 headersSig = [
-	"m(C1)","m(N1)","dm", "channel","NTrees","MNS","Depth","nSig","nBkg",
+	"m(C1)","m(N1)","dm", "channel","NTrees","MNS","Depth","ROC-int",
+	"nSig","nBkg",
 	"nSig(0)","nBkg(0)","CLs(0)",
 	"nSig(0.1)","nBkg(0.1)","CLs(0.1)",
 	"nSig(0.2)","nBkg(0.2)","CLs(0.2)",
-	"nSig(0.3)","nBkg(0.3)","CLs(0.3)"
+	"nSig(0.3)","nBkg(0.3)","CLs(0.3)",
+	"BDTopt", "CLs(BDTopt)"
 	]
 
 '''
@@ -54,9 +57,9 @@ def makeSampleIdDB():
 		if "MGPy8EG_A14N23LO_C1N2_Slep" not in aLine: continue
 
 		sampleID = int((re.search(".[0-9]{6}.", aLine).group()).split('.')[1])
-		masses = re.search("Slep_[0-9]{3,4}_[0-9]{1,4}", aLine).group()
-		mC1 = int(masses.split('_')[1])
-		mN1 = int(masses.split('_')[2])
+		masses = re.search("Slep_[0-9]{3,4}_[0-9]{1,4}", aLine).group().split('_')
+		mC1 = int(masses[1])
+		mN1 = int(masses[2])
 
 		sampleIdDB[(mC1, mN1)] = sampleID
 	return sampleIdDB
@@ -155,26 +158,36 @@ if __name__ == '__main__':
 				os.rename("plots/overtrain_BDTD.eps", overtrainPlotname)
 			else: open(overtrainPlotname, 'a').close()						
 
-			rowsOT.append( [dm, chan, ntrees, mns, depth] + [roc] + [passOT] + ["%s" % str(r) for r in ratios ] + list(overtrainingNums))
+			rowsOT.append( (dm, chan, ntrees, mns, depth, roc, passOT) + tuple(["%s" % str(r) for r in ratios] ) + overtrainingNums)
 
 			if passOT==" ": continue
 
 			## CLs checks
 			for mass in tqdm(C1masses):
+				if mass==1000 and dm%100!=0: continue # Only (1000, 100n) samples available
 				nSig = nSigDict.get(sampleIdDB[(mass, mass-dm)],{}).get(chan,0)
 				if nSig < 1: continue
 				nBkg = nBkgTotDict[chan]
 
 				call('root -l -b -q "mvaeffs.cxx(\\"%s/%s\\", %d, %f)"' % (directory, file, int(nSig), int(nBkg)), shell=True)
 				
+				minCLs = (1, 1)
 				with open("effs.csv") as effFile:
 					reader = csv.reader(effFile)
-					effList = list(reader)[0][:-12] # Only extract nSig, nBkg for BDT = 0, 0.1, 0.2, 0.3
-				rowsSig.append( [mass, mass-dm, dm, chan, ntrees, mns, depth, nSig, nBkg] + effList) 
+					effList = list(reader)[0][6:] # Only extract nSig, nBkg for BDT = 0, 0.1, 0.2, 0.3
+					for i in range(0,12,3):
+						# CLs formula is PP(nData|S+B)/PP(nBkg|B)
+						nSig = float(effList[i])
+						nBkg = float(effList[i+1])
+						nData = int(nBkg) # Take nData = nBkg for this MC study
+						CLs = PP(nData, nSig+nBkg) / PP(nData, nBkg)
+						effList[i+2] = CLs
+						if minCLs[1]>CLs: minCLs = (round(i/30.,1), CLs) 
+				rowsSig.append( (mass, mass-dm, dm, chan, ntrees, mns, depth, roc, nSig, nBkg) + tuple(effList[:-1]) + minCLs) 
 				os.rename("plots/mvaeffs_BDTD.eps", "%s/%d_%d_Channel%d.eps" % (plotDir, mass, mass-dm, chan))
 
 	rowsOT = sorted(rowsOT, key=lambda x : (x[0], x[1], -x[5]) )
-	rowsSig = sorted(rowsSig, key=lambda x: (x[2], x[0], max(x[-1], x[-4], x[-7], x[-10])))
+	rowsSig = sorted(rowsSig, key=lambda x: (x[2], x[0], x[-1]))
 
 
 	# Export to CSV
@@ -184,6 +197,39 @@ if __name__ == '__main__':
 	for line in rowsSig:
 		outSig.write(",".join(["%s" % str(l) for l in line]))
 		outSig.write("\n")
+
+	# Plot graph
+	c = ROOT.TCanvas()
+
+	h = ROOT.TH2D("minCLs", "Minimum CLs;m(C1) [GeV];m(N1) [GeV]", 50, 100, 1000, 41, 80, 900)
+
+	currentMass = (0, 0)
+	for line in rowsSig:
+		if currentMass == (line[0], line[1]): continue
+		currentMass = (line[0], line[1])
+		h.Fill(line[0], line[1], line[-1])
+		print (line[0], line[1], line[-1])
+	h.Draw("text")
+
+	line = ROOT.TLine()
+	l0 = line.DrawLine(100, 100, 900, 900)
+	l0.SetLineStyle(1)
+	l0.SetLineWidth(1)
+
+	l20 = line.DrawLine(100, 80, 920, 900)
+	l20.SetLineStyle(3)
+	l20.SetLineWidth(1)
+
+	l50 = line.DrawLine(130, 80, 950, 900)
+	l50.SetLineStyle(3)
+	l50.SetLineWidth(1)
+
+	l100 = line.DrawLine(180, 80, 1000, 900)
+	l100.SetLineStyle(3)
+	l100.SetLineWidth(1)
+
+	c.Print("minCLs.pdf")
+
 
 
 
