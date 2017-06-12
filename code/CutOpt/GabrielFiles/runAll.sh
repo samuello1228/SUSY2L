6e3fe23af38c3ea49712480cde5275c74b63e043
Gabriel Gallardo <gabriel.gallardo@cern.ch>
2017-06-12 09:49:14 +0800

    Update runAll.sh

--- code/CutOpt/GabrielFiles/runAll.sh
+++ code/CutOpt/GabrielFiles/runAll.sh
@@ -5,17 +5,33 @@
 # masses="20 50 100"
 # masses1="all 20"
 # masses2="50 100 +"
-nTreesList="25 50 75 100 200"
-nodeSizeList="5 10 15"
-depthList="2 3 4"
 
 
 # channels="3 13"
 # masses="20 50"
 
+# FOR DM=20
 masses="20"
-channels="1 2 4" 
-channels1="10 11 12 14" 
+# channels="1 2 4" 
+# channels1="10 11 12 14" 
+# channels="3 13"
+# nTreesList="25 50 75 100"
+# nodeSizeList="5 10 15"
+# depthList="2 3 4"
+
+# FOR DM=50
+# masses="50"
+# channels="3 13" 
+# nTreesList="100 200 400"
+# nodeSizeList="5 7 10"
+# depthList="2 3 4"
+
+# FOR DM=100
+masses="100"
+channels="3 13" 
+nTreesList="400"
+nodeSizeList="5"
+depthList="3"
 
 bkgFiles="CutOpt/GabrielFiles/MCbkgFiles.txt"
 dataFiles="CutOpt/GabrielFiles/data2016.txt"
@@ -39,37 +55,37 @@ do
 		done
 		# wait
 	done
-	# wait
+	 wait
 
 	# Channel 0 
-	for mass in $masses
-	do 
-		outFile="dm"$mass"_Channel0"
-		sigFiles="CutOpt/GabrielFiles/sigFiles_"$mass".txt"
-		python CutOpt/optWithTMVA.py -b $bkgFiles -s $sigFiles -d $dataFiles -c 0 -o $outFile".root" --NTrees $nTrees --NodeSize $nodeSize --Depth $depth > $outFile".log"&
-		echo "Submitted jobs for channel 0 dm="$mass" nTrees="$nTrees" nodeSize="$nodeSize" depth="$depth
-	done 
+	# for mass in $masses
+	# do 
+	# 	outFile="dm"$mass"_Channel0"
+	# 	sigFiles="CutOpt/GabrielFiles/sigFiles_"$mass".txt"
+	# 	python CutOpt/optWithTMVA.py -b $bkgFiles -s $sigFiles -d $dataFiles -c 0 -o $outFile".root" --NTrees $nTrees --NodeSize $nodeSize --Depth $depth > $outFile".log"&
+	# 	echo "Submitted jobs for channel 0 dm="$mass" nTrees="$nTrees" nodeSize="$nodeSize" depth="$depth
+	# done 
 
-	wait
+	# wait
 
-	for chan in $channels1
-	do
-		for mass in $masses
-		do
-			if [ $mass -eq "100" ]
-			then	
-				chan1=`expr $chan + 100`
-			else 
-				chan1=$chan
-			fi
-		    outFile="dm"$mass"_Channel"$chan1
-		    sigFiles="CutOpt/GabrielFiles/sigFiles_"$mass".txt"
-		    python CutOpt/optWithTMVA.py -b $bkgFiles -s $sigFiles -d $dataFiles -c $chan1 -o $outFile".root" --NTrees $nTrees --NodeSize $nodeSize --Depth $depth > $outFile".log"&
-		    echo "Submitted jobs for channel "$chan1" dm="$mass" nTrees="$nTrees" nodeSize="$nodeSize" depth="$depth
-		done
-		# wait
-	done
-	wait
+	# for chan in $channels1
+	# do
+	# 	for mass in $masses
+	# 	do
+	# 		if [ $mass -eq "100" ]
+	# 		then	
+	# 			chan1=`expr $chan + 100`
+	# 		else 
+	# 			chan1=$chan
+	# 		fi
+	# 	    outFile="dm"$mass"_Channel"$chan1
+	# 	    sigFiles="CutOpt/GabrielFiles/sigFiles_"$mass".txt"
+	# 	    python CutOpt/optWithTMVA.py -b $bkgFiles -s $sigFiles -d $dataFiles -c $chan1 -o $outFile".root" --NTrees $nTrees --NodeSize $nodeSize --Depth $depth > $outFile".log"&
+	# 	    echo "Submitted jobs for channel "$chan1" dm="$mass" nTrees="$nTrees" nodeSize="$nodeSize" depth="$depth
+	# 	done
+	# 	# wait
+	# done
+	# wait
 
 	dirName="Output_"$nTrees"_NodeSize"$nodeSize"_Depth"$depth
 	mkdir $dirName; mkdir $dirName"/weights"
