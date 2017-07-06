21ec820e7a22ea319910365268c7bf6e64b178a6
Gabriel Gallardo <gabriel.gallardo@cern.ch>
2017-07-06 11:39:35 +0200

    Scripts and support files for fakes run

--- code/multiLepSearch/script/Data_submit.sh
+++ code/multiLepSearch/script/Data_submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tag=v9.3
+tag=v11
 grl=GoodRunsLists/data16_13TeV/20170215/physics_25ns_20.7.xml,GoodRunsLists/data15_13TeV/20160720/physics_25ns_20.7.xml
 dataPRW=GoodRunsLists/data16_13TeV/20170215/physics_25ns_20.7.lumicalc.OflLumi-13TeV-008.root,GoodRunsLists/data15_13TeV/20160720/physics_25ns_20.7.lumicalc.OflLumi-13TeV-005.root
 SUSYconf=multiLepSearch/sel_conf/SUSYTools_multilepAnaMoriond.conf
@@ -10,4 +10,4 @@ mcPRW=dev/SUSYTools/merged_prw_mc15c_Jan31.root
 
 file=../multiLepSearch/script/Data_sample_list.txt
 k=${tag}.data 
-../multiLepSearch/util/run_ss_selection.py --driver grid --inputList ${file} --dataPRW ${dataPRW} --mcPRW ${mcPRW} --grl ${grl} --outputTag ${tag} -o ${k} -w -a 0 --study ss --doSys 0  --conf ${SUSYconf}
+../multiLepSearch/util/run_ss_selection.py --driver grid --inputList ${file} --dataPRW ${dataPRW} --mcPRW ${mcPRW} --grl ${grl} --outputTag ${tag} -o ${k} -w -a 0 --study fakes --doSys 0  --conf ${SUSYconf}
--- code/multiLepSearch/script/MC_submit.sh
+++ code/multiLepSearch/script/MC_submit.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-tag=v9.3
+tag=v11
 dataPRW=GoodRunsLists/data16_13TeV/20170215/physics_25ns_20.7.lumicalc.OflLumi-13TeV-008.root,GoodRunsLists/data15_13TeV/20160720/physics_25ns_20.7.lumicalc.OflLumi-13TeV-005.root
 mcPRW=dev/SUSYTools/merged_prw_mc15c_Jan31.root,multiLepSearch/prw_MC/merged_prw_mc15c_Slep0d95.root,dev/PileupReweighting/mc15c_v2_defaults.NotRecommended.prw.root
-SUSYconf=multiLepSearch/sel_conf/SUSYTools_multilepAnaMoriond.conf
+# SUSYconf=multiLepSearch/sel_conf/SUSYTools_multilepAnaMoriond.conf
+SUSYconf=multiLepSearch/sel_conf/SUSYTools_Default.conf
 
 #For background MC
 #mcPRW=dev/SUSYTools/merged_prw_mc15c_latest.root
 
-file=../multiLepSearch/script/MCBG_sample_list.txt,../multiLepSearch/script/MCBGZjetsSherpa_sample_list.txt,../multiLepSearch/script/MCBGVVSherpa_sample_list.txt,../multiLepSearch/script/MCBGVgammaSherpa_sample_list.txt,../multiLepSearch/script/MCBGDYSherpa_sample_list.txt
+# file=../multiLepSearch/script/MCBG_sample_list.txt,../multiLepSearch/script/MCBGZjetsSherpa_sample_list.txt,../multiLepSearch/script/MCBGVVSherpa_sample_list.txt,../multiLepSearch/script/MCBGVgammaSherpa_sample_list.txt,../multiLepSearch/script/MCBGDYSherpa_sample_list.txt
 #file=../multiLepSearch/script/MCBG_sample_list.txt
 #file=../multiLepSearch/script/MCBGZjetsSherpa_sample_list.txt
 #file=../multiLepSearch/script/MCBGVVSherpa_sample_list.txt
 #file=../multiLepSearch/script/MCBGVgammaSherpa_sample_list.txt
 #file=../multiLepSearch/script/MCBGDYSherpa_sample_list.txt
+file=../multiLepSearch/script/MCFakes_sample_list.txt
 
 #file=../multiLepSearch/script/MCBGZeeSherpaSelected.txt
 #file=../multiLepSearch/script/MCBG_llll.txt
@@ -24,4 +26,4 @@ k=${tag}.MCBG
 #file=../multiLepSearch/script/MCSig_sample_list_new.txt
 #k=${tag}.MCSig
 
-../multiLepSearch/util/run_ss_selection.py --driver grid --inputList ${file} --dataPRW ${dataPRW} --mcPRW ${mcPRW} --outputTag ${tag} -o ${k} -w -a 1 --study ss --mcMatch dR --doSys 0 --ChargeID 1 --conf ${SUSYconf}
+../multiLepSearch/util/run_ss_selection.py --driver grid --inputList ${file} --dataPRW ${dataPRW} --mcPRW ${mcPRW} --outputTag ${tag} -o ${k} -w -a 1 --study fakes --mcMatch dR --doSys 0 --ChargeID 1 --conf ${SUSYconf}
