23d417ff8e03d2293b7eb766624fdbb2679027ea
Gabriel Gallardo <gabriel.gallardo@cern.ch>
2017-07-20 04:28:00 +0200

    Update file lists and submit scripts

--- code/multiLepSearch/script/MC_submit.sh
+++ code/multiLepSearch/script/MC_submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tag=v11.1
+tag=v11.2
 dataPRW=GoodRunsLists/data16_13TeV/20170215/physics_25ns_20.7.lumicalc.OflLumi-13TeV-008.root,GoodRunsLists/data15_13TeV/20160720/physics_25ns_20.7.lumicalc.OflLumi-13TeV-005.root
 mcPRW=dev/SUSYTools/merged_prw_mc15c_Jan31.root,multiLepSearch/prw_MC/merged_prw_mc15c_Slep0d95.root,dev/PileupReweighting/mc15c_v2_defaults.NotRecommended.prw.root
 # SUSYconf=multiLepSearch/sel_conf/SUSYTools_multilepAnaMoriond.conf
@@ -14,8 +14,8 @@ SUSYconf=multiLepSearch/sel_conf/SUSYTools_Default.conf
 #file=../multiLepSearch/script/MCBGVVSherpa_sample_list.txt
 #file=../multiLepSearch/script/MCBGVgammaSherpa_sample_list.txt
 #file=../multiLepSearch/script/MCBGDYSherpa_sample_list.txt
-file=../multiLepSearch/script/MCFakes_sample_list.txt
-
+# file=../multiLepSearch/script/MCFakes_sample_list.txt
+file=../multiLepSearch/script/MCZPowheg.txt
 #file=../multiLepSearch/script/MCBGZeeSherpaSelected.txt
 #file=../multiLepSearch/script/MCBG_llll.txt
 
