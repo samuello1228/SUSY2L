b83486d3f5770d91d11fb7b689756522c26d84df
Samuel <locheukyee2001@gmail.com>
2018-09-22 03:32:33 +0200

    compareDani: add cutflow

--- code/multiLepSearch/util/compareDani.cxx
+++ code/multiLepSearch/util/compareDani.cxx
@@ -96,9 +96,10 @@ int main()
     TChain* tree2 = new TChain("evt2l");
     //tree2->Add("/afs/cern.ch/user/c/clo/AnalysisBase-02-04-31-test/SUSY2L/code/run/t1_old/data-myOutput/test.root");
     //tree2->Add("/afs/cern.ch/user/c/clo/AnalysisBase-02-04-31-test/SUSY2L/code/run/t1/data-myOutput/test.root");
-    TString path2 = "/eos/user/c/clo/ntuple/AnalysisBase-02-04-31-c1d5464d/user.clo.v13.0.";
-    tree2->Add(path2 + "VV_CT10_myOutput.root/user.clo.mc15_13TeV.361071.*.myOutput.root*");
-    tree2->Add(path2 + "VV_221_myOutput.root/user.clo.mc15_13TeV.363491.*.myOutput.root*");
+    //TString path2 = "/eos/user/c/clo/ntuple/AnalysisBase-02-04-31-c1d5464d/user.clo.v13.0.";
+    TString path2 = "/eos/user/c/clo/ntuple/AnalysisBase-02-04-31-new/user.clo.v13.2.";
+    tree2->Add(path2 + "VV_CT10_myOutput.root/user.clo.mc15_13TeV.361071.*.myOutput.root*"); int sampleID = 361071; double XS = 0.042287*0.91;
+    //tree2->Add(path2 + "VV_221_myOutput.root/user.clo.mc15_13TeV.363491.*.myOutput.root*"); int sampleID = 363491;
     susyEvts* evt = new susyEvts(tree2);
     cout<<"Our ntuple has events: "<<tree2->GetEntries()<<endl;
 
@@ -132,123 +133,246 @@ int main()
     //cutflowList.push_back("");
     //cutflowList.push_back(",w");
 
-    TH1D* hCutflow = new TH1D("cutflow", "cutflow", cutflowList.size(), 0, cutflowList.size());
+    TH1D* hCutflow_Dani = new TH1D("cutflow_Dani", "cutflow_Dani", cutflowList.size(), 0, cutflowList.size());
+    TH1D* hCutflow_Samuel = new TH1D("cutflow_Samuel", "cutflow_Samuel", cutflowList.size(), 0, cutflowList.size());
     for(unsigned int i=0;i<cutflowList.size();i++)    
     {
-        hCutflow->GetXaxis()->SetBinLabel(i+1,cutflowList[i].Data());
+        hCutflow_Dani->GetXaxis()->SetBinLabel(i+1,cutflowList[i].Data());
+        hCutflow_Samuel->GetXaxis()->SetBinLabel(i+1,cutflowList[i].Data());
     }
 
-    /*
+    int SR = 1;
+    //SR = 2;
+
+    //cutflow for Dani ntuple
     //for (long i = 0; i < 100000; i++)
     for (long i = 0; i < tree1->GetEntries(); i++)
     {
         tree1->GetEntry(i);
 
+        if(MCId != sampleID) continue;
         if(NlepBL !=2) continue;
         if(nSigLep !=2) continue;
         double weight = totweight*lumiScaling;
-        hCutflow->Fill("=2BaseLep && =2SigLep && SS",1);
-        hCutflow->Fill("=2BaseLep && =2SigLep && SS,w",weight);
+        hCutflow_Dani->Fill("=2BaseLep && =2SigLep && SS",1);
+        hCutflow_Dani->Fill("=2BaseLep && =2SigLep && SS,w",weight);
 
         if(isTruthLep1 != 1) continue;
-        hCutflow->Fill("isTruthLep1",1);
-        hCutflow->Fill("isTruthLep1,w",weight);
+        hCutflow_Dani->Fill("isTruthLep1",1);
+        hCutflow_Dani->Fill("isTruthLep1,w",weight);
 
         if(isTruthLep2 != 1) continue;
-        hCutflow->Fill("isTruthLep2",1);
-        hCutflow->Fill("isTruthLep2,w",weight);
+        hCutflow_Dani->Fill("isTruthLep2",1);
+        hCutflow_Dani->Fill("isTruthLep2,w",weight);
 
-        //int SR = 1;
-        int SR = 1;
         if(SR == 1)
         {
             if(nJets20 !=1) continue;
-            hCutflow->Fill("nJet",1);
-            hCutflow->Fill("nJet,w",weight);
+            hCutflow_Dani->Fill("nJet",1);
+            hCutflow_Dani->Fill("nJet,w",weight);
         }
         else if(SR == 2)
         {
             if(nJets20 !=2 && nJets20 !=3) continue;
-            hCutflow->Fill("nJet",1);
-            hCutflow->Fill("nJet,w",weight);
+            hCutflow_Dani->Fill("nJet",1);
+            hCutflow_Dani->Fill("nJet,w",weight);
         }
 
         if(nBJets20 !=0) continue;
-        hCutflow->Fill("nBJets20",1);
-        hCutflow->Fill("nBJets20,w",weight);
+        hCutflow_Dani->Fill("nBJets20",1);
+        hCutflow_Dani->Fill("nBJets20,w",weight);
 
         if(Pt_l < 25000) continue;
-        hCutflow->Fill("pt1",1);
-        hCutflow->Fill("pt1,w",weight);
+        hCutflow_Dani->Fill("pt1",1);
+        hCutflow_Dani->Fill("pt1,w",weight);
 
         if(Pt_subl < 25000) continue;
-        hCutflow->Fill("pt2",1);
-        hCutflow->Fill("pt2,w",weight);
+        hCutflow_Dani->Fill("pt2",1);
+        hCutflow_Dani->Fill("pt2,w",weight);
 
         if(SR == 1)
         {
             if(fabs(DeltaEtaLep)>1.5) continue;
-            hCutflow->Fill("DeltaEtaLep",1);
-            hCutflow->Fill("DeltaEtaLep,w",weight);
+            hCutflow_Dani->Fill("DeltaEtaLep",1);
+            hCutflow_Dani->Fill("DeltaEtaLep,w",weight);
 
             if(met<100000) continue;
-            hCutflow->Fill("met",1);
-            hCutflow->Fill("met,w",weight);
+            hCutflow_Dani->Fill("met",1);
+            hCutflow_Dani->Fill("met,w",weight);
 
             if(mt<140000) continue;
-            hCutflow->Fill("mt",1);
-            hCutflow->Fill("mt,w",weight);
+            hCutflow_Dani->Fill("mt",1);
+            hCutflow_Dani->Fill("mt,w",weight);
 
             if(meff<260000) continue;
-            hCutflow->Fill("meff",1);
-            hCutflow->Fill("meff,w",weight);
+            hCutflow_Dani->Fill("meff",1);
+            hCutflow_Dani->Fill("meff,w",weight);
 
             if(mljj_comb>=180000) continue;
-            hCutflow->Fill("mljj",1);
-            hCutflow->Fill("mljj,w",weight);
+            hCutflow_Dani->Fill("mljj",1);
+            hCutflow_Dani->Fill("mljj,w",weight);
 
             if(MT2<80000) continue;
-            hCutflow->Fill("MT2",1);
-            hCutflow->Fill("MT2,w",weight);
+            hCutflow_Dani->Fill("MT2",1);
+            hCutflow_Dani->Fill("MT2,w",weight);
         }
         else if(SR == 2)
         {
-            hCutflow->Fill("DeltaEtaLep",1);
-            hCutflow->Fill("DeltaEtaLep,w",weight);
+            hCutflow_Dani->Fill("DeltaEtaLep",1);
+            hCutflow_Dani->Fill("DeltaEtaLep,w",weight);
 
             if(met<100000) continue;
-            hCutflow->Fill("met",1);
-            hCutflow->Fill("met,w",weight);
+            hCutflow_Dani->Fill("met",1);
+            hCutflow_Dani->Fill("met,w",weight);
 
             if(mt<120000) continue;
-            hCutflow->Fill("mt",1);
-            hCutflow->Fill("mt,w",weight);
+            hCutflow_Dani->Fill("mt",1);
+            hCutflow_Dani->Fill("mt,w",weight);
 
             if(meff<240000) continue;
-            hCutflow->Fill("meff",1);
-            hCutflow->Fill("meff,w",weight);
+            hCutflow_Dani->Fill("meff",1);
+            hCutflow_Dani->Fill("meff,w",weight);
 
             if(mljj_comb>=130000) continue;
-            hCutflow->Fill("mljj",1);
-            hCutflow->Fill("mljj,w",weight);
+            hCutflow_Dani->Fill("mljj",1);
+            hCutflow_Dani->Fill("mljj,w",weight);
 
             if(MT2<70000) continue;
-            hCutflow->Fill("MT2",1);
-            hCutflow->Fill("MT2,w",weight);
+            hCutflow_Dani->Fill("MT2",1);
+            hCutflow_Dani->Fill("MT2,w",weight);
         }
-
-        //cout<<"MCID: "<<MCId<<endl;
-        //cout<<"isTruthLep1: "<<isTruthLep1<<endl;
-        //cout<<"isTruthLep2: "<<isTruthLep2<<endl;
     }
 
     for(unsigned int j=1;j<=cutflowList.size();j++)
     {
-        cout<<hCutflow->GetXaxis()->GetBinLabel(j)<<": ";
-        if(j%2 == 1) cout<<int(hCutflow->GetBinContent(j))<<endl;
-        if(j%2 == 0) cout<<std::fixed<<std::setprecision(6)<<hCutflow->GetBinContent(j)<<endl;
+        cout<<hCutflow_Dani->GetXaxis()->GetBinLabel(j)<<": ";
+        if(j%2 == 1) cout<<int(hCutflow_Dani->GetBinContent(j))<<endl;
+        if(j%2 == 0) cout<<std::fixed<<std::setprecision(10)<<hCutflow_Dani->GetBinContent(j)<<endl;
     }
-    */
+
+    double nwAOD = 0;
+    TObjArray* fileList = tree2->GetListOfFiles();
+    for(int i=0;i<fileList->GetEntries();i++)
+    {
+        cout<<fileList->At(i)->GetTitle()<<endl;
+        TFile *f1 = TFile::Open(fileList->At(i)->GetTitle());
+        TH1D *h1 = (TH1D*) f1->Get("hCutFlow");
+        //for(unsigned int j=1;j<30;j++) icout<<j<<" "<<h1->GetBinContent(j)<<endl;;
+        nwAOD += h1->GetBinContent(2);
+        
+        delete f1;
+    }
+
+    double lumi = 36100;
+    double commonWeight = XS *lumi /nwAOD;
+    //cutflow for My ntuple
+    for (long i = 0; i < tree2->GetEntries(); i++)
+    {
+        tree2->GetEntry(i);
+
+        if(evt->sig.nSigLep < 2) continue;
+        if(!evt->sig.isSS) continue;
+        if(!evt->sig.JetCut) continue;
+        if(evt->sig.isZ) continue;
+
+        if(evt->sig.nBaseLep !=2) continue;
+        if(evt->sig.nSigLep !=2) continue;
+        double weight = evt->evt.weight * evt->evt.pwt * evt->evt.trigSF * evt->evt.ElSF * evt->evt.MuSF * evt->evt.BtagSF * evt->evt.JvtSF * commonWeight;
+        hCutflow_Samuel->Fill("=2BaseLep && =2SigLep && SS",1);
+        hCutflow_Samuel->Fill("=2BaseLep && =2SigLep && SS,w",weight);
+
+        if(evt->leps[0].lepTruth != 1) continue;
+        hCutflow_Samuel->Fill("isTruthLep1",1);
+        hCutflow_Samuel->Fill("isTruthLep1,w",weight);
+
+        if(evt->leps[1].lepTruth != 1) continue;
+        hCutflow_Samuel->Fill("isTruthLep2",1);
+        hCutflow_Samuel->Fill("isTruthLep2,w",weight);
+
+        if(SR == 1)
+        {
+            if(evt->sig.nJet !=1) continue;
+            hCutflow_Samuel->Fill("nJet",1);
+            hCutflow_Samuel->Fill("nJet,w",weight);
+        }
+        else if(SR == 2)
+        {
+            if(evt->sig.nJet !=2 && evt->sig.nJet !=3) continue;
+            hCutflow_Samuel->Fill("nJet",1);
+            hCutflow_Samuel->Fill("nJet,w",weight);
+        }
+
+        if(evt->sig.nBJet !=0) continue;
+        hCutflow_Samuel->Fill("nBJets20",1);
+        hCutflow_Samuel->Fill("nBJets20,w",weight);
+
+        if(evt->leps[0].pt < 25) continue;
+        hCutflow_Samuel->Fill("pt1",1);
+        hCutflow_Samuel->Fill("pt1,w",weight);
+
+        if(evt->leps[1].pt < 25) continue;
+        hCutflow_Samuel->Fill("pt2",1);
+        hCutflow_Samuel->Fill("pt2,w",weight);
+
+        if(SR == 1)
+        {
+            if(fabs(evt->leps[0].eta - evt->leps[1].eta) > 1.5) continue;
+            hCutflow_Samuel->Fill("DeltaEtaLep",1);
+            hCutflow_Samuel->Fill("DeltaEtaLep,w",weight);
+
+            if(evt->sig.Met<100) continue;
+            hCutflow_Samuel->Fill("met",1);
+            hCutflow_Samuel->Fill("met,w",weight);
+
+            if(evt->leps[0].mT<140) continue;
+            hCutflow_Samuel->Fill("mt",1);
+            hCutflow_Samuel->Fill("mt,w",weight);
+
+            if(evt->sig.HT + evt->sig.Met < 260) continue;
+            hCutflow_Samuel->Fill("meff",1);
+            hCutflow_Samuel->Fill("meff,w",weight);
+
+            if(evt->sig.mlj>=180) continue;
+            hCutflow_Samuel->Fill("mljj",1);
+            hCutflow_Samuel->Fill("mljj,w",weight);
+
+            if(evt->sig.mT2<80) continue;
+            hCutflow_Samuel->Fill("MT2",1);
+            hCutflow_Samuel->Fill("MT2,w",weight);
+        }
+        else if(SR == 2)
+        {
+            hCutflow_Samuel->Fill("DeltaEtaLep",1);
+            hCutflow_Samuel->Fill("DeltaEtaLep,w",weight);
+
+            if(evt->sig.Met<100) continue;
+            hCutflow_Samuel->Fill("met",1);
+            hCutflow_Samuel->Fill("met,w",weight);
+
+            if(evt->leps[0].mT<120) continue;
+            hCutflow_Samuel->Fill("mt",1);
+            hCutflow_Samuel->Fill("mt,w",weight);
+
+            if(evt->sig.HT + evt->sig.Met < 240) continue;
+            hCutflow_Samuel->Fill("meff",1);
+            hCutflow_Samuel->Fill("meff,w",weight);
+
+            if(evt->sig.mlj>=130) continue;
+            hCutflow_Samuel->Fill("mljj",1);
+            hCutflow_Samuel->Fill("mljj,w",weight);
+
+            if(evt->sig.mT2<70) continue;
+            hCutflow_Samuel->Fill("MT2",1);
+            hCutflow_Samuel->Fill("MT2,w",weight);
+        }
+    }
+
+    for(unsigned int j=1;j<=cutflowList.size();j++)
+    {
+        cout<<hCutflow_Samuel->GetXaxis()->GetBinLabel(j)<<": ";
+        if(j%2 == 1) cout<<int(hCutflow_Samuel->GetBinContent(j))<<endl;
+        if(j%2 == 0) cout<<std::fixed<<std::setprecision(10)<<hCutflow_Samuel->GetBinContent(j)<<endl;
+    }
 
     int j = 0;
     int count = 0;
@@ -336,7 +460,8 @@ int main()
     }
     cout<<"Total number of event in Dani selection: "<<count<<endl;
 
-    delete hCutflow;
+    delete hCutflow_Dani;
+    delete hCutflow_Samuel;
     delete tree1;
 }
 
