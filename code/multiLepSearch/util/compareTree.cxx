7607bf99aaab2c081c57d802abaa926f64b79934
Samuel <locheukyee2001@gmail.com>
2018-09-24 22:02:47 +0200

    compareTree: add isSR

--- code/multiLepSearch/util/compareTree.cxx
+++ code/multiLepSearch/util/compareTree.cxx
@@ -89,77 +89,97 @@ struct pt_index
 
 int main()
 {
+    bool isSR = true;
+    bool isCR = false;
+
     //Peter
-    TString path1 = "/afs/cern.ch/user/c/clo/AnalysisBase-02-04-31-Peter1/Output_old/data-output/mc15_13TeV.361071.Sherpa_CT10_lllvjj_EW6.merge.DAOD_SUSY2.e3836_s2726_r7772_r7676_p2949.root";
-    const TString treeName1 = "DiLeptonTree_SRall";
+    TString path1 = "";
+    TString treeName1 = "";
+    if(isSR)
+    {
+        path1 = "/afs/cern.ch/user/c/clo/AnalysisBase-02-04-31-Peter1/Output_s2/data-output/mc15_13TeV.361071.Sherpa_CT10_lllvjj_EW6.merge.DAOD_SUSY2.e3836_s2726_r7772_r7676_p2949.root";
+        treeName1 = "DiLeptonTree_SRall";
+    }
+    else if(isCR)
+    {
+        path1 = "/afs/cern.ch/user/c/clo/AnalysisBase-02-04-31-Peter2/Output_normal/data-output/mc15_13TeV.361071.Sherpa_CT10_lllvjj_EW6.merge.DAOD_SUSY2.e3836_s2726_r7772_r7676_p2949.root";
+        treeName1 = "DiLeptonTree_CR";
+    }
+
     TChain* tree1 = new TChain(treeName1);
     tree1->Add(path1.Data());
     cout<<"tree1 has events: "<<tree1->GetEntries()<<endl;
 
     //configure input branches
-    tree1->SetBranchAddress("EventNumber",  &EventNumber);
-    tree1->SetBranchAddress("RunNumber",    &RunNumber);
-    tree1->SetBranchAddress("PileUpHash",   &PileUpHash);
-    tree1->SetBranchAddress("xAODWeightSum", &xAODWeightSum);
-    tree1->SetBranchAddress("RandomRN",     &randomRN);
-    tree1->SetBranchAddress("ConfigYear",   &configYear);
-    tree1->SetBranchAddress("MCId",         &MCId);
-    tree1->SetBranchAddress("MCWeight",     &EventWeight);
-    tree1->SetBranchAddress("PileUpWeight", &PileUpWeight);
-    tree1->SetBranchAddress("ObjectSF",     &ObjectSF);
-    tree1->SetBranchAddress("TriggerSF",    &triggerSF);
-    tree1->SetBranchAddress("ChargeFlipSF", &chargeFlipSF);
-    tree1->SetBranchAddress("TotalWeight",  &weight);
-    tree1->SetBranchAddress("EtMiss",       &EtMiss);
-    tree1->SetBranchAddress("EtMissPhi",    &EtMissPhi);
-    tree1->SetBranchAddress("Meff",         &meff);
-    tree1->SetBranchAddress("Minv",         &minv);
-    tree1->SetBranchAddress("Mt",           &mt);
-    tree1->SetBranchAddress("Ht",           &ht);
-    tree1->SetBranchAddress("Mj",           &mj);
-    tree1->SetBranchAddress("Mjj",          &mjj);
-    tree1->SetBranchAddress("MjjdR",        &mjjdR);
-    tree1->SetBranchAddress("MjRec",        &mjRec);
-    tree1->SetBranchAddress("TruthHt",      &truthHt);
-    tree1->SetBranchAddress("TruthMET",     &truthMET);
-    tree1->SetBranchAddress("Zevent",       &isZevent);
-    tree1->SetBranchAddress("nW",           &nW);
-    tree1->SetBranchAddress("NlepBL",       &NlepBL);
-    tree1->SetBranchAddress("NlepSig",      &NlepSig);
-    tree1->SetBranchAddress("NlepBLEta",    &NlepBLEta);
-    tree1->SetBranchAddress("Njet25",       &Njet25);
-    tree1->SetBranchAddress("Njet35",       &Njet35);
-    tree1->SetBranchAddress("Njet40",       &Njet40);
-    tree1->SetBranchAddress("Njet50",       &Njet50);
-    tree1->SetBranchAddress("Nbjet",        &Nbjet);
-    tree1->SetBranchAddress("jetBtag",      &jetBtag);
-    tree1->SetBranchAddress("jetTLV",       &jetTLV);
-    tree1->SetBranchAddress("lepTLV",       &lepTLV);
-    tree1->SetBranchAddress("lepTLV_BL",    &lepTLV_BL);
-    tree1->SetBranchAddress("lepZ0",        &lepZ0);
-    tree1->SetBranchAddress("lepCharges",   &lepCharges);
-    tree1->SetBranchAddress("lepCFmedium",  &lepCFmedium);
-    tree1->SetBranchAddress("lepCFloose",   &lepCFloose);
-    tree1->SetBranchAddress("lepTruth",     &lepTruth);
-    tree1->SetBranchAddress("SSChannel",    &SSChannel);
-    tree1->SetBranchAddress("isSameSign",   &SameSign);
-    tree1->SetBranchAddress("SysNames",     &SysNames);
-    tree1->SetBranchAddress("SysWeights",   &SysWeights);
-    tree1->SetBranchAddress("PDFWeights",   &PDFWeights);
-    tree1->SetBranchAddress("VarWeights",   &VarWeights);
-    tree1->SetBranchAddress("CFTWeights",   &CFTWeights);
-    tree1->SetBranchAddress("TrigWeights",  &TrigWeights);
-    tree1->SetBranchAddress("triggerInfo",  &triggerInfo);
-    tree1->SetBranchAddress("PDGId1",       &PDGId1);
-    tree1->SetBranchAddress("PDGId2",       &PDGId2);
-    tree1->SetBranchAddress("GLDec1",       &GLDec1);
-    tree1->SetBranchAddress("GLDec2",       &GLDec2);
-    tree1->SetBranchAddress("IntPerX",      &IntPerX);
+    if(isSR)
+    {
+        tree1->SetBranchAddress("EventNumber",  &EventNumber);
+        //tree1->SetBranchAddress("RunNumber",    &RunNumber);
+        //tree1->SetBranchAddress("PileUpHash",   &PileUpHash);
+        //tree1->SetBranchAddress("xAODWeightSum", &xAODWeightSum);
+        //tree1->SetBranchAddress("RandomRN",     &randomRN);
+        //tree1->SetBranchAddress("ConfigYear",   &configYear);
+        //tree1->SetBranchAddress("MCId",         &MCId);
+        tree1->SetBranchAddress("MCWeight",     &EventWeight);
+        tree1->SetBranchAddress("PileUpWeight", &PileUpWeight);
+        tree1->SetBranchAddress("ObjectSF",     &ObjectSF);
+        tree1->SetBranchAddress("TriggerSF",    &triggerSF);
+        tree1->SetBranchAddress("ChargeFlipSF", &chargeFlipSF);
+        tree1->SetBranchAddress("TotalWeight",  &weight);
+        tree1->SetBranchAddress("EtMiss",       &EtMiss);
+        tree1->SetBranchAddress("EtMissPhi",    &EtMissPhi);
+        tree1->SetBranchAddress("Meff",         &meff);
+        tree1->SetBranchAddress("Minv",         &minv);
+        tree1->SetBranchAddress("Mt",           &mt);
+        tree1->SetBranchAddress("Ht",           &ht);
+        //tree1->SetBranchAddress("Mj",           &mj);
+        //tree1->SetBranchAddress("Mjj",          &mjj);
+        //tree1->SetBranchAddress("MjjdR",        &mjjdR);
+        //tree1->SetBranchAddress("MjRec",        &mjRec);
+        //tree1->SetBranchAddress("TruthHt",      &truthHt);
+        //tree1->SetBranchAddress("TruthMET",     &truthMET);
+        //tree1->SetBranchAddress("Zevent",       &isZevent);
+        //tree1->SetBranchAddress("nW",           &nW);
+        tree1->SetBranchAddress("NlepBL",       &NlepBL);
+        tree1->SetBranchAddress("NlepSig",      &NlepSig);
+        //tree1->SetBranchAddress("NlepBLEta",    &NlepBLEta);
+        //tree1->SetBranchAddress("Njet25",       &Njet25);
+        //tree1->SetBranchAddress("Njet35",       &Njet35);
+        //tree1->SetBranchAddress("Njet40",       &Njet40);
+        //tree1->SetBranchAddress("Njet50",       &Njet50);
+        tree1->SetBranchAddress("Nbjet",        &Nbjet);
+        tree1->SetBranchAddress("jetBtag",      &jetBtag);
+        tree1->SetBranchAddress("jetTLV",       &jetTLV);
+        tree1->SetBranchAddress("lepTLV",       &lepTLV);
+        tree1->SetBranchAddress("lepTLV_BL",    &lepTLV_BL);
+        //tree1->SetBranchAddress("lepZ0",        &lepZ0);
+        tree1->SetBranchAddress("lepCharges",   &lepCharges);
+        //tree1->SetBranchAddress("lepCFmedium",  &lepCFmedium);
+        //tree1->SetBranchAddress("lepCFloose",   &lepCFloose);
+        tree1->SetBranchAddress("lepTruth",     &lepTruth);
+        //tree1->SetBranchAddress("SSChannel",    &SSChannel);
+        //tree1->SetBranchAddress("isSameSign",   &SameSign);
+        //tree1->SetBranchAddress("SysNames",     &SysNames);
+        //tree1->SetBranchAddress("SysWeights",   &SysWeights);
+        //tree1->SetBranchAddress("PDFWeights",   &PDFWeights);
+        //tree1->SetBranchAddress("VarWeights",   &VarWeights);
+        //tree1->SetBranchAddress("CFTWeights",   &CFTWeights);
+        //tree1->SetBranchAddress("TrigWeights",  &TrigWeights);
+        //tree1->SetBranchAddress("triggerInfo",  &triggerInfo);
+        //tree1->SetBranchAddress("PDGId1",       &PDGId1);
+        //tree1->SetBranchAddress("PDGId2",       &PDGId2);
+        //tree1->SetBranchAddress("GLDec1",       &GLDec1);
+        //tree1->SetBranchAddress("GLDec2",       &GLDec2);
+        //tree1->SetBranchAddress("IntPerX",      &IntPerX);
+    }
+    else if(isCR)
+    {
+    }
 
     //Our ntuple
     TChain* tree2 = new TChain("evt2l");
-    //tree2->Add("/afs/cern.ch/user/c/clo/AnalysisBase-02-04-31-test/SUSY2L/code/run/t1_old/data-myOutput/test.root");
-    tree2->Add("/afs/cern.ch/user/c/clo/AnalysisBase-02-04-31-test/SUSY2L/code/run/t1/data-myOutput/test.root");
+    tree2->Add("/afs/cern.ch/user/c/clo/AnalysisBase-02-04-31/SUSY2L/code/run/t1_s2/data-myOutput/test.root");
+    //tree2->Add("/afs/cern.ch/user/c/clo/AnalysisBase-02-04-31/SUSY2L/code/run/t1/data-myOutput/test.root");
     susyEvts* evt = new susyEvts(tree2);
     cout<<"tree2 has events: "<<tree2->GetEntries()<<endl;
 
